5ad06e925a59c00c1510df15af4400584f692263
lvcuong112 <nguyenvutrung2010@gmail.com>
2021-07-20 17:17:31 +0700

    item and rock

--- public/game.js
+++ public/game.js
@@ -10,7 +10,24 @@ const ctx = canvas.getContext('2d');
 
 let score = 0;
 let gameFrame = 0;
+
+ console.log("image loaded");
+
 ctx.font = '50px Georgia'
+// background
+
+class Background{
+    constructor(){
+        this.x = 0;
+        this.y = 0;
+        this.Image = new Image();
+        this.Image.src = "./Image/map.png";
+    }
+    draw(){
+        ctx.drawImage(this.Image, this.x, this.y, canvas.width, canvas.height);
+    }
+}
+const background = new Background();
 //controller
 
 const controller = {
@@ -105,7 +122,7 @@ class Car{
        
         ctx.fillStyle = 'red';
         ctx.beginPath();
-        ctx.arc(0, 0, this.radius, 0, Math.PI * 2);
+        //ctx.arc(0, 0, this.radius, 0, Math.PI * 2);
         ctx.fill();
         ctx.closePath();
         ctx.drawImage(this.img, -47, -22);
@@ -116,16 +133,76 @@ class Car{
 const car = new Car();
 //item
 
+const itemArray = [];
+class Item{
+    constructor(){
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height; 
+        this.radius = 25;
+        this.distance = 0;
+        this.timeToLive = 0;
+        this.img = new Image();
+        this.img.src = "./Image/Golden.png"
+
+    }
+    update(){
+        //this.y -= this.speed;
+        const dx = this.x - car.x;
+        const dy = this.y - car.y;
+        this.distance = Math.sqrt(dx * dx + dy * dy);
+    }
+    draw(){
+        ctx.fillStyle = 'yellow';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+        ctx.drawImage(this.img, this.x - 25, this.y - 25);
+        ctx.stroke();
+    }
+
+
+
+}
+
+function handleItem(){
+    if (gameFrame % 100 == 0){
+        if (itemArray.length < 3){
+            itemArray.push(new Item());
+        }
+    }
+
+    for (i = 0; i < itemArray.length; i++){
+        itemArray[i].update();
+        itemArray[i].draw();
+    }
+    for (i = 0; i < itemArray.length; i++){
+        if (itemArray[i].timeToLive > 1000){
+            itemArray.splice(i , 1);
+            continue;
+        } else {
+        if (itemArray[i].distance < itemArray[i].radius + car.radius + 8){
+            itemArray.splice(i, 1);    
+            score++;  
+            continue;
+        }
+        itemArray[i].timeToLive++;
+    }
+
+
+    }
+
+}
 //bubles
 const bubblesArray = [];
 class Bubble {
     constructor(){
         this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height + canvas.height + 100;
+        this.y = Math.random() * canvas.height; 
         this.radius = 50;
         this.speed = Math.random() * 5 + 1;
         this.distance = 0;
-        this.counted = false;
+        this.counted = 0;
         this.img1 = new Image();
         this.img1.src = './Image/rock_type_planet.png'
         this.img2 = new Image();
@@ -133,7 +210,7 @@ class Bubble {
     }
 
     update(){
-        this.y -= this.speed;
+        //this.y -= this.speed;
         const dx = this.x - car.x;
         const dy = this.y - car.y;
         this.distance = Math.sqrt(dx * dx + dy * dy);
@@ -143,48 +220,60 @@ class Bubble {
     draw(){
         ctx.fillStyle = 'blue';
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        //ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.fill();
         ctx.closePath();
         ctx.stroke();
-        if (gameFrame % 2 == 0){
+        
             ctx.drawImage(this.img1, this.x - this.radius, this.y - this.radius);
-        } else {
-            ctx.drawImage(this.img2, this.x - this.radius, this.y - this.radius);
-        }
+        
+        //    ctx.drawImage(this.img2, this.x - this.radius, this.y - this.radius);
+        
     }
 }
 function handleBubbles(){
     if (gameFrame % 50 == 0){
+        if (bubblesArray.length <7){
         bubblesArray.push(new Bubble());
         console.log(bubblesArray.length);
+        }
     }
     for (i = 0; i < bubblesArray.length; i++){
         bubblesArray[i].update();
         bubblesArray[i].draw();  
     }
     for (i = 0; i < bubblesArray.length; i++){
-        if (bubblesArray[i].y + 100 < 0){
-            bubblesArray.splice(i, 1);
-        }
-        if (bubblesArray[i].distance < bubblesArray[i].radius + car.radius){
-            if (!bubblesArray[i].counted){
-                score++;
-                bubblesArray[i].counted = true;
+        
+        if (bubblesArray[i].distance < bubblesArray[i].radius + car.radius + 8){
+            if (bubblesArray[i].counted > 5){
+                //score++;
+                //bubblesArray[i].counted = true;
                 bubblesArray.splice(i , 1);
+                continue;
             }
-           
+            bubblesArray[i].counted++;
+            var speedX = car.speed * Math.cos(car.degree);
+            var speedY = car.speed * Math.sin(car.degree);
+            if (controller.upKeyIsPressed){
+                car.x -= speedX * 4;
+                car.y -= speedY * 4;
+            } else {
+                car.x += speedX * 4;
+                car.y += speedY * 4;   
+            }
         }
 
 
-}
+    }
 }
 //loop
 
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    background.draw();
     car.update();
     handleBubbles();
+    handleItem();
     car.draw();
     ctx.fillStyle = 'black';
     ctx.fillText('Score ' + score, 10, 50);
